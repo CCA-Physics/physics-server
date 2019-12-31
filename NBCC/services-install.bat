1763ccb02e2c3070eef93fc87e7a2403bfe44a60
SimonBiggs <sbiggs@riverinacancercare.com.au>
2019-12-31 17:10:51 +1100

    updated nbcc tunnel

--- NBCC/services-install.bat
+++ NBCC/services-install.bat
@@ -1,10 +1,12 @@
+SET PATH="C:\Users\Public\Documents\physics-server\bin";%PATH%
+
 nssm install tunnels tunnels.bat
 
-nssm set tunnels Application C:\Users\Public\Documents\physics-server\tunnels.bat
-nssm set tunnels AppDirectory C:\Users\Public\Documents\physics-server
+nssm set tunnels Application C:\Users\Public\Documents\physics-server\NBCC\tunnels.bat
+nssm set tunnels AppDirectory C:\Users\Public\Documents\physics-server\NBCC
 
-nssm set tunnels AppStdout C:\Users\Public\Documents\physics-server\log-tunnels.txt
-nssm set tunnels AppStdout C:\Users\Public\Documents\physics-server\log-tunnels.txt
+nssm set tunnels AppStdout C:\Users\Public\Documents\physics-server\NBCC\tunnels_log.txt
+nssm set tunnels AppStdout C:\Users\Public\Documents\physics-server\NBCC\tunnels_log.txt
 
 nssm set tunnels AppRestartDelay 60000
 
