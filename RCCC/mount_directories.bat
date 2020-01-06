5eaa5bf19b6bc49aca96dab50ba2b9d5fc747ff4
CCA-Physics <sbiggs@riverinacancercare.com.au>
2020-01-06 05:01:43 +0000

    more startup bats

---
+++ RCCC/mount_directories.bat
@@ -0,0 +1,7 @@
+timeout /t 120
+
+net use Y: \\10.0.0.3\SASH-DataExchange /PERSISTENT:YES
+net use Z: \\10.0.0.4\Physics\NBCC-DataExchange /PERSISTENT:YES
+
+net use W: \\10.0.0.5\Users\Public\Documents\CMS\FocalData /PERSISTENT:YES
+net use X: \\10.0.0.6\focaldata /PERSISTENT:YES
