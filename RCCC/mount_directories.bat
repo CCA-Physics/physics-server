64573c360eb78718210c0964b19e3eeff2eeb0f7
CCA-Physics <sbiggs@riverinacancercare.com.au>
2020-01-07 04:30:03 +0000

    allow websockets for dosecheck, and new dir mount

--- RCCC/mount_directories.bat
+++ RCCC/mount_directories.bat
@@ -5,3 +5,5 @@ net use Z: \\10.0.0.4\Physics\NBCC-DataExchange /PERSISTENT:YES
 
 net use W: \\10.0.0.5\Users\Public\Documents\CMS\FocalData /PERSISTENT:YES
 net use X: \\10.0.0.6\focaldata /PERSISTENT:YES
+
+net use V: \\10.0.0.3\SASH-Mosaiq-eScan /PERSISTENT:YES
