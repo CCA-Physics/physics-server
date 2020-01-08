e5dd6288b53972188c7a9d4e9c0d1e353089c552
CCA-Physics <sbiggs@riverinacancercare.com.au>
2020-01-08 01:29:08 +0000

    delete boo

--- RCCC/boo.bat
+++
@@ -1,4 +0,0 @@
-FOR /F "tokens=*" %g IN ('python -c "import keyring; print(keyring.get_password('ssh server', 'pexit'))"') do (SET PASSWORD=%g)
-SET COMMAND= "echo %PASSWORD% | sudo -S service ssh start"
-
-bash -c %COMMAND%
