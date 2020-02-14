293001de5734f3a85b5d633906ce0a4d30e5b45f
CCA-Physics <sbiggs@riverinacancercare.com.au>
2020-02-14 04:51:45 +0000

    formatting

--- SASH/services-install.bat
+++ SASH/services-install.bat
@@ -4,8 +4,10 @@ nssm install tunnels tunnels.bat
 
 nssm set tunnels Application C:\Users\Public\Documents\physics-server\SASH\tunnels.bat
 nssm set tunnels AppDirectory C:\Users\Public\Documents\physics-server\SASH
+
 nssm set tunnels AppStdout C:\Users\Public\Documents\physics-server\SASH\tunnels_log.txt
 nssm set tunnels AppStderr C:\Users\Public\Documents\physics-server\SASH\tunnels_log.txt
+
 nssm set tunnels AppRestartDelay 60000
 
 PAUSE
