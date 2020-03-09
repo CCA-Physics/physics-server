064c7d40ffbf3984e6e42ca7b7db699c7e7d6226
CCA-Physics <sbiggs@riverinacancercare.com.au>
2020-03-09 03:58:41 +0000

    add sufficient pauses

--- RCCC/between_site_file_mover.bat
+++ RCCC/between_site_file_mover.bat
@@ -1,2 +1,3 @@
+timeout /t 240
 python ./python/watch_and_transfer_files.py
 PAUSE
--- RCCC/icom/patient_archiving.bat
+++ RCCC/icom/patient_archiving.bat
@@ -1,1 +1,2 @@
 pymedphys icom archive D:\iComLogFiles\live Z:\iCom\live --by-patient --output-dir D:\iComLogFiles\patients
+PAUSE
