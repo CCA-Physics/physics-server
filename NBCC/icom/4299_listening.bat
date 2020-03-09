60e646d6c738fe4006760c4f6daace99579098a1
SimonBiggs <sbiggs@riverinacancercare.com.au>
2020-03-09 15:45:25 +1100

    use vendored Python

--- NBCC/icom/4299_listening.bat
+++ NBCC/icom/4299_listening.bat
@@ -1,1 +1,5 @@
-pymedphys icom listen 192.168.17.40 \\NBCCC-pdc\physics\NBCC-DataExchange\iCom\live
+cd C:\Users\Public\Documents\python
+SET PATH="C:\Users\Public\Documents\python";"C:\Users\Public\Documents\python\Scripts";%PATH%
+C:\Users\Public\Documents\python\Scripts\pymedphys.exe icom listen 192.168.17.40 \\NBCCC-pdc\physics\NBCC-DataExchange\iCom
+
+PAUSE
