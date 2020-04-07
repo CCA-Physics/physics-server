79e7f00f0c58a3f3f39c3ed4553846b9cac02bfe
SimonBiggs <sbiggs@riverinacancercare.com.au>
2020-04-07 12:09:40 +1000

    add icom to SASH

---
+++ SASH/icom/9002_listening.bat
@@ -0,0 +1,5 @@
+SET PYTHON_DIR="C:\Users\Administrator\Anaconda3"
+cd %PYTHON_DIR%
+SET PATH="%PYTHON_DIR%";"%PYTHON_DIR%\Scripts";%PATH%
+
+pymedphys icom listen 192.168.40.10 C:\Users\Public\Documents\DataTransfer\icom
