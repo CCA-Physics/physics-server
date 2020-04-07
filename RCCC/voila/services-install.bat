de1e7346c264e76999b61997311422b6f0eefd3b
CCA-Physics <sbiggs@riverinacancercare.com.au>
2020-04-07 23:16:51 +0000

    fix image viewer forwarding through nginx

--- RCCC/voila/services-install.bat
+++ RCCC/voila/services-install.bat
@@ -5,8 +5,8 @@ nssm install voila_servers voila_servers.bat
 nssm set voila_servers Application C:\Users\Public\Documents\physics-server\RCCC\voila\voila_servers.bat
 nssm set voila_servers AppDirectory C:\Users\Public\Documents\physics-server\RCCC\voila
 
-nssm set voila_servers AppStdout C:\Users\Public\Documents\physics-server\RCCC\voila\voila-log.txt
-nssm set voila_servers AppStderr C:\Users\Public\Documents\physics-server\RCCC\voila\voila-log.txt
+nssm set voila_servers AppStdout C:\Users\Public\Documents\physics-server\RCCC\voila\voila_log.txt
+nssm set voila_servers AppStderr C:\Users\Public\Documents\physics-server\RCCC\voila\voila_log.txt
 
 nssm set voila_servers AppRestartDelay 300000
 
