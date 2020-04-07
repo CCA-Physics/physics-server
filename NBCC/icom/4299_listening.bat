4f1bb0d04e1da6d91bc430e59e55ccbda0495b93
SimonBiggs <sbiggs@riverinacancercare.com.au>
2020-04-07 11:35:26 +1000

    update 4299 listening

--- NBCC/icom/4299_listening.bat
+++ NBCC/icom/4299_listening.bat
@@ -1,3 +1,5 @@
 cd C:\Users\Public\Documents\python
-SET PATH="C:\Users\Public\Documents\python";"C:\Users\Public\Documents\python\Scripts";%PATH%
+SET PATH="C:\Users\Public\Documents\python;C:\Users\Public\Documents\python\Scripts;%PATH%"
 C:\Users\Public\Documents\python\Scripts\pymedphys.exe icom listen 192.168.17.40 \\NBCCC-pdc\physics\NBCC-DataExchange\iCom
+
+PAUSE
