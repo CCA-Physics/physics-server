0c48ffce0a6d8e7bbd6824a55d0a158c3313af6c
CCA-Physics <sbiggs@riverinacancercare.com.au>
2020-05-26 23:36:07 +0000

    remove unneeded bat files

--- RCCC/icom/patient_archiving.bat
+++
@@ -1,7 +0,0 @@
-timeout /t 240
-C:
-cd C:\Users\PExIT\git\pymedphys
-
-poetry run pymedphys icom archive D:\iComLogFiles\live Z:\iCom\live Y:\icom\live --by-patient --output-dir D:\iComLogFiles\patients
-
-PAUSE
