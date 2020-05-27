906bbd2421e9286ea4e4e022415446fa06ab65d3
SimonBiggs <sbiggs@riverinacancercare.com.au>
2020-05-27 13:35:34 +1000

    bump to 0.30.0dev0

--- SASH/icom/install.bat
+++ SASH/icom/install.bat
@@ -2,6 +2,6 @@ SET PYTHON_DIR="C:\Users\Administrator\Anaconda3"
 cd %PYTHON_DIR%
 SET PATH="%PYTHON_DIR%";"%PYTHON_DIR%\Scripts";%PATH%
 
-pip install pymedphys==0.28.0 --no-deps
+pip install pymedphys==0.30.0dev0 --no-deps
 
 PAUSE
