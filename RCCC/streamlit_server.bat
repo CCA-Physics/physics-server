45d8fe38645bd1c827f5cf4a52c0c13f025f05af
CCA-Physics <sbiggs@riverinacancercare.com.au>
2020-08-05 05:46:22 +0000

    add other servers to streamlit

--- RCCC/streamlit_server.bat
+++ RCCC/streamlit_server.bat
@@ -1,7 +1,6 @@
 C:
-SET PYTHON_DIR="C:\Users\PExIT\.pyenv\pyenv-win\versions\3.7.5-amd64"
-"%PYTHON_DIR%";"%PYTHON_DIR%\Scripts";"C:\Program Files\ImageMagick-7.0.9-Q16";"%PATH%"
+cd C:\Users\PExIT\git\pymedphys
 
-pymedphys expermiental gui
+poetry run pymedphys experimental gui
 
 PAUSE
