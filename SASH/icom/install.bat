b3080ba0c803db79b208e4b9521ed6830b3e5f88
SimonBiggs <sbiggs@riverinacancercare.com.au>
2020-08-11 13:52:24 +1000

    update pymedphys installation

--- SASH/icom/install.bat
+++ SASH/icom/install.bat
@@ -2,6 +2,7 @@ SET PYTHON_DIR="C:\Users\Administrator\Anaconda3"
 cd %PYTHON_DIR%
 SET PATH="%PYTHON_DIR%";"%PYTHON_DIR%\Scripts";%PATH%
 
-pip install pymedphys==0.30.0dev0 --no-deps
+pip install numpy
+pip install pymedphys==0.31.0dev2 --no-deps
 
 PAUSE
