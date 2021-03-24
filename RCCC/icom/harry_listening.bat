8a7636d47138bf5700088d2de52a0e9288a74efd
CCA-Physics <sbiggs@riverinacancercare.com.au>
2021-03-24 01:01:11 +0000

    update harry and peter listening

--- RCCC/icom/harry_listening.bat
+++ RCCC/icom/harry_listening.bat
@@ -1,1 +1,5 @@
+SET PYTHON_DIR="C:\Users\PExIT\AppData\Local\pypoetry\Cache\virtualenvs\pymedphys-q3DRi5lT-py3.8"
+cd %PYTHON_DIR%
+SET PATH=%PYTHON_DIR%;%PYTHON_DIR%\Scripts
+
 pymedphys icom listen 192.168.100.201 D:\iComLogFiles
--- RCCC/icom/peter_listening.bat
+++ RCCC/icom/peter_listening.bat
@@ -1,1 +1,5 @@
+SET PYTHON_DIR="C:\Users\PExIT\AppData\Local\pypoetry\Cache\virtualenvs\pymedphys-q3DRi5lT-py3.8"
+cd %PYTHON_DIR%
+SET PATH=%PYTHON_DIR%;%PYTHON_DIR%\Scripts
+
 pymedphys icom listen 192.168.100.200 D:\iComLogFiles
