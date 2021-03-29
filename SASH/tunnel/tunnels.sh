84929e836ba5c5b8ea20c35b5338c62adda6e695
SimonBiggs <sbiggs@riverinacancercare.com.au>
2021-03-29 14:13:45 +1100

    add commenting to tunnels script

--- SASH/tunnel/tunnels.sh
+++ SASH/tunnel/tunnels.sh
@@ -1,11 +1,13 @@
 ssh -N -o ServerAliveInterval=248 pexit@165.228.124.29 -p 38471 \
-  -L 0.0.0.0:80:localhost:80 \
-  -L 0.0.0.0:34242:localhost:34242 \
-  -L 0.0.0.0:4242:localhost:4242 \
-  -R 0.0.0.0:1433:sash-mosaiq:1433 \
-  -R 0.0.0.0:44445:192.168.40.10:445 \
-  -R 0.0.0.0:44447:localhost:445 \
-  -R 0.0.0.0:44449:elektatps:445 \
-  -R 0.0.0.0:34104:elektatps:104 \
-  -R 0.0.0.0:43104:192.168.40.10:104 \
-  -L 0.0.0.0:34567:localhost:34567 \
+  `# Forwarding from RCCC to SASH` \
+  -L 0.0.0.0:80:localhost:80 `# http://physics-server/, http://dosecheck.physics-server/, and http://dicom.physics-server/` \
+  -L 0.0.0.0:34242:localhost:34242 `# orthanc-bounce DICOM receiver` \
+  -L 0.0.0.0:4242:localhost:4242 `# orthanc-physics DICOM receiver` \
+  -L 0.0.0.0:34567:localhost:34567 `# Mathew Cooper's Vacbag DICOM receiver` \
+  `# Forwarding from SASH to RCCC` \
+  -R 0.0.0.0:1433:sash-mosaiq:1433 `# SASH Mosaiq SQL` \
+  -R 0.0.0.0:44445:192.168.40.10:445 `# SAMBA \\tunnel-sash-9002` \
+  -R 0.0.0.0:44447:localhost:445 `# SAMBA \\tunnel-sash-physics-server` \
+  -R 0.0.0.0:44449:elektatps:445 `# SAMBA \\tunnel-sash-monaco` \
+  -R 0.0.0.0:34104:elektatps:104 `# Monaco's DICOM receiver` \
+  -R 0.0.0.0:43104:192.168.40.10:104 `# 9002 service mode DICOM receiver`
