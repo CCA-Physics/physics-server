9faaa7e3c02616414c179a7a3d594e044e6018cd
CCA-Physics <physics@riverinacancercare.com.au>
2021-03-30 09:10:02 +1100

    fetch and index with more granular logs

--- RCCC/trigger_fetch_and_index.bat
+++ RCCC/trigger_fetch_and_index.bat
@@ -1,12 +1,16 @@
 SET PYMEDPHYS_DIR=C:\Users\PExIT\git\pymedphys
 SET TRF_DIR=D:\LinacTRFBackup
-SET LOG=%TRF_DIR%\running-log.txt
+
+FOR /F "tokens=* USEBACKQ" %%F IN (`powershell -c "get-date -format yyyy-MM-dd--HH-mm-ss"`) DO (
+    SET DATETIME=%%F
+)
+SET LOG=%TRF_DIR%\task-scheduler-logs\%DATETIME%.log.txt
 
 C:
 cd %PYMEDPHYS_DIR%
 echo %DATE% %TIME% >> %LOG%
 poetry run pymedphys trf orchestrate >> %LOG% 2>&1
 
-NET USE W: \\utilsvr\PhysBack
-xcopy D:\LinacTRFBackup W:\LinacTRFBackup /E /G /H /D /Y
+NET USE W: \\utilsvr\PhysBack >> %LOG% 2>&1
+xcopy D:\LinacTRFBackup W:\LinacTRFBackup /E /G /H /D /Y >> %LOG% 2>&1
 echo %DATE% %TIME% >> %LOG%
